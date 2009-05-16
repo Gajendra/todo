6d33be06858022a4695c2a3b64552418e79b66f5
griefworker <griefworker@gmail.com>
2009-05-17 08:06:48 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090516224510) do
+ActiveRecord::Schema.define(:version => 20090516225320) do
+
+  create_table "items", :force => true do |t|
+    t.string   "title"
+    t.boolean  "done"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
