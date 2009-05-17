7bb00d8ede5f4d44ddfb3d046e9a66dd02477845
griefworker <griefworker@gmail.com>
2009-05-17 09:15:40 +0900

    add link_to_tag method to items_helper

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,6 @@
 module ItemsHelper
+  # ƒ^ƒO‚Åi‚è‚ŞƒŠƒ“ƒN‚ğì¬‚µ‚Ü‚·
+  def link_to_tag(tag)
+    link_to(tag, { :controller => "items", :action => "index", :tag => tag }, :rel => "tag")
+  end
 end
